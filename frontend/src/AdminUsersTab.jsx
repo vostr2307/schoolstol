f3bb75cce29a0f5ddafd13a8574568a85ddf062f
vostr2307 <vo_ostr@mail.ru>
2025-05-23 13:33:11 +0300

    Update AdminUsersTab.jsx

--- frontend/src/AdminUsersTab.jsx
+++ frontend/src/AdminUsersTab.jsx
@@ -8,7 +8,7 @@ export default function AdminUsersTab() {
   const [editingUser, setEditingUser] = useState(null);
 
   const loadUsers = () => {
-    fetch('/users')
+    fetch(API_URL + '/users')
       .then(res => res.json())
       .then(setUsers);
   };
